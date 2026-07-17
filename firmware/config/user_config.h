f3c8809a5bacb20f21f6b989c18379d7ef7b5fee
Mingma Tendu Sherpa <106554475+M-Tendu-Sherpa@users.noreply.github.com>
2026-07-16 19:43:21 -0500

    Update user_config.h

--- firmware/config/user_config.h
+++ firmware/config/user_config.h
@@ -1,1 +1,7 @@
+#pragma once
 
+#define DEVICE_NAME              "DA14531_SLEEP_DEV"
+#define DEFAULT_SESSION_DURATION_SEC   (8 * 60 * 60)   // default of 8 hours
+#define ADC_SAMPLE_PERIOD_MS           100             // sampling interval
+#define ADC_THRESHOLD_MV               5               // minimum delta to log
+#define MAX_LOG_SAMPLES                4096
