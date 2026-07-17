6950fa19327cabad8975a750e8be90f8abce8db3
Mingma Tendu Sherpa <106554475+M-Tendu-Sherpa@users.noreply.github.com>
2026-07-16 19:56:30 -0500

    Update app_adc.c

--- firmware/src/app_adc.c
+++ firmware/src/app_adc.c
@@ -1,1 +1,15 @@
+#include "app_adc.h"
+#include "adc_config.h"
+#include "sdk_adc_driver.h"
 
+void app_adc_init(void)
+{
+    sdk_adc_init(ADC_CHANNEL_0, ADC_REF_INTERNAL, ADC_ATTEN_NONE    // Configure ADC
+}
+
+bool app_adc_read_mv(uint16_t *value_mv)
+{
+    uint16_t raw = sdk_adc_read();
+    *value_mv = sdk_adc_raw_to_mv(raw);
+    return true;
+}
